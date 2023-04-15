15cde15a2adf359647a306452b2b04808bd4aa25
Jefe21054(Ivan) <ivaniglesias500@hotmail.com>
2023-04-15 01:15:25 -0500

    Introducción a Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from models import User, db_user, get_user
-from forms import LoginForm, SignupForm
+from forms import LoginForm, SignupForm, AddCourseForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "so-secret"
@@ -47,6 +47,42 @@ def signin():
 @login_required
 def dashboard():
     return render_template("dashboard.html")
+
+@app.route("/dashboard/user/account")
+@login_required
+def user_account():
+    return render_template("user_account.html")
+
+@app.route("/dashboard/courses/add", methods=["GET", "POST"])
+@login_required
+def add_courses():
+    form = AddCourseForm()
+    if form.validate_on_submit():
+        return redirect(url_for("dashboard"))
+    return render_template("add_course.html", form=form)
+
+@app.route("/dashboard/courses")
+@login_required
+def courses():
+    courses = [{"professor": "Kevin Guzmán", 
+                "title": "Fundamentos de Flask",
+                "description": "Es un curso donde aprenderemos conceptos básicos del desarrollo utilizando la excelente herramienta de FLASK",
+                "url": "https://ed.team/cursos/flask"}, 
+                {"professor": "Diego Adrian Barra Paredes", 
+                "title": "Curso: React Hooks con TypeScript",
+                "description": "A menudo cuando escribes componentes dentro de las clases, te encuentras que con el tiempo se vuelven complejos, difíciles de organizar, y la lógica de estado entre los componentes no la puedes reutilizar.",
+                "url": "https://ed.team/cursos/react-hooks"}]
+    return render_template("courses.html", courses=courses)
+
+@app.route("/dashboard/course/delete/<id>")
+@login_required
+def delete_course(id=None):
+    return f"Deleted Course with id {id}"
+
+@app.route("/dashboard/course/update/<id>")
+@login_required
+def update_course(id=None):
+    return f"Updated Course with id {id}"
 
 @app.route("/logout")
 @login_required
