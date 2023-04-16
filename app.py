e4d3b143d20aaea8f1df74a2d5e34eb1d5fa2663
Jefe21054(Ivan) <ivaniglesias500@hotmail.com>
2023-04-15 21:07:32 -0500

    Flask-Migrate para actualizar nuestra base de datos

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from forms import LoginForm, SignupForm, AddCourseForm
 
 app = Flask(__name__)
@@ -11,6 +12,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 from models import User, Courses
 
+migrate = Migrate(app, db)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'signin'
